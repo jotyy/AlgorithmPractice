d11c999e0efabd5d83759ba05cbfa46093def64f
Jotyy <838373812@qq.com>
2020-11-06 01:14:35 +0800

    DAY4: TreeDepth.kt

---
+++ src/main/kotlin/binarytree/TreeDepth.kt
@@ -0,0 +1,34 @@
+package binarytree
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class TreeDepth {
+    /**
+     * 深度优先搜索
+     */
+    private fun minDepth(root: TreeNode?): Int {
+        root?.let {
+            if (it.left == null && it.right == null) {
+                return 1
+            }
+
+            var depth = Int.MAX_VALUE
+            if (it.left != null) {
+                depth = Math.min(minDepth(it.left), depth)
+            }
+            if (it.right != null) {
+                depth = Math.min(minDepth(it.right), depth)
+            }
+
+            return depth + 1
+        } ?: return 0
+    }
+}
