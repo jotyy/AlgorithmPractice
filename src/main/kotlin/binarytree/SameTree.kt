8824e7cb3306dce29ef007ec067ff9b4489c6504
Jotyy <838373812@qq.com>
2020-11-06 00:40:35 +0800

    DAY4: SameTree.kt

---
+++ src/main/kotlin/binarytree/SameTree.kt
@@ -0,0 +1,25 @@
+package binarytree
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class SameTree {
+    private fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        return if (p == null && q == null) {
+            true
+        } else if (p == null || q == null) {
+            false
+        } else if (p.`val` != q.`val`) {
+            false
+        } else {
+            isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+        }
+    }
+}
